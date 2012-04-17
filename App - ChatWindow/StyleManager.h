aadb5605a3e5db00f523e99ac0b596bef06fab44
Martin Conte Mac Donell <Reflejo@gmail.com>
2012-04-17 04:01:31 -0300

    Working full customizable HTML messages

---
+++ App - ChatWindow/StyleManager.h
@@ -0,0 +1,13 @@
+//
+//  StyleManager.h
+//  FacebookChat
+//
+//  Created by Usuario Vacio on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StyleManager : NSObject
+
+@end
---
+++ App - ChatWindow/StyleManager.m
@@ -0,0 +1,13 @@
+//
+//  StyleManager.m
+//  FacebookChat
+//
+//  Created by Usuario Vacio on 4/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "StyleManager.h"
+
+@implementation StyleManager
+
+@end
